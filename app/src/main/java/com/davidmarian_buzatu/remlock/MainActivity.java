c67689b33c79bd1afb572608fe10c6a224a453f3
lestherll <ljllacuna5@gmail.com>
2020-11-15 13:56:02 +0000

    Added event to open Calendar

--- app/src/main/java/com/davidmarian_buzatu/remlock/MainActivity.java
+++ app/src/main/java/com/davidmarian_buzatu/remlock/MainActivity.java
@@ -2,14 +2,63 @@ package com.davidmarian_buzatu.remlock;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.CalendarContract;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText title;
+    EditText location;
+    EditText description;
+    Button addEvent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        title = findViewById(R.id.eventTitle);
+        location = findViewById(R.id.eventLocation);
+        description = findViewById(R.id.eventDescription);
+        addEvent = findViewById(R.id.btnAdd);
+
+        addEvent.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Check if event form is filled
+                // Currently all of the fields must be filled for the
+                // program to open Calendar, this can be changed later
+                if (!title.getText().toString().isEmpty() && !location.getText().toString().isEmpty()
+                        && !description.getText().toString().isEmpty()) {
+
+                    Intent intent = new Intent(Intent.ACTION_INSERT);
+                    intent.setData(CalendarContract.Events.CONTENT_URI);
+                    intent.putExtra(CalendarContract.Events.TITLE, title.getText().toString());
+                    intent.putExtra(CalendarContract.Events.EVENT_LOCATION, location.getText().toString());
+                    intent.putExtra(CalendarContract.Events.DESCRIPTION, description.getText().toString());
+                    intent.putExtra(CalendarContract.Events.ALL_DAY, true);
+                    intent.putExtra(Intent.EXTRA_EMAIL, "test@yahoo.com, test2@yahoo.com");
+
+                    // Open app if form is filled and has permission
+                    if (intent.resolveActivity(getPackageManager()) != null) {
+                        startActivity(intent);
+                    }
+                    else {
+                        Toast.makeText(MainActivity.this, "There is no app that can support this action",
+                                Toast.LENGTH_SHORT).show();
+                    }
+
+                }
+                else {
+                    Toast.makeText(MainActivity.this, "enter details",
+                            Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
