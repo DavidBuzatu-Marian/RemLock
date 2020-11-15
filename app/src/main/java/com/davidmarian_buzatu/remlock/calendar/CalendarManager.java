4a76797a9275ce59ebefcad419e6a94240073c74
lestherll <ljllacuna5@gmail.com>
2020-11-15 14:49:02 +0000

    Added class for managing calendar

---
+++ app/src/main/java/com/davidmarian_buzatu/remlock/calendar/CalendarManager.java
@@ -0,0 +1,10 @@
+package com.davidmarian_buzatu.remlock.calendar;
+
+import com.davidmarian_buzatu.remlock.MainActivity;
+import com.davidmarian_buzatu.remlock.R;
+
+public class CalendarManager {
+
+
+
+}
