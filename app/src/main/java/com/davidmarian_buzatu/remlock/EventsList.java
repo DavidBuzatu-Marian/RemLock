031db5fce0dc3191b69b83c2f136a552b712d538
davidbuzatu-marian <davidm.buzatu@gmail.com>
2020-11-15 17:28:56 +0000

    (main) Added popup

--- app/src/main/java/com/davidmarian_buzatu/remlock/EventsList.java
+++ app/src/main/java/com/davidmarian_buzatu/remlock/EventsList.java
@@ -2,23 +2,28 @@ package com.davidmarian_buzatu.remlock;
 
 import android.content.Context;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.PopupWindow;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.RecyclerView;
 
+import static android.content.Context.LAYOUT_INFLATER_SERVICE;
+
 public class EventsList extends RecyclerView.Adapter<EventsList.EventsViewHolder> {
 
     private String[] data;
     private Context context;
-    public EventsList(String[] data, Context context)
-    {
+
+    public EventsList(String[] data, Context context) {
         this.data = data;
         this.context = context;
     }
@@ -42,11 +47,12 @@ public class EventsList extends RecyclerView.Adapter<EventsList.EventsViewHolder
         return data.length;
     }
 
-    public static class EventsViewHolder extends RecyclerView.ViewHolder{
+    public static class EventsViewHolder extends RecyclerView.ViewHolder {
         private ImageView imgIcon;
         private TextView txtTitle;
         private Button viewDetails;
         private Context eventContext;
+
         public EventsViewHolder(View itemView, Context context) {
             super(itemView);
             imgIcon = itemView.findViewById(R.id.imgIcon);
@@ -62,8 +68,25 @@ public class EventsList extends RecyclerView.Adapter<EventsList.EventsViewHolder
                 public void onClick(View view) {
                     Log.d("FIRED", "FIRED");
                     imgIcon.setBackgroundResource(R.drawable.rectangle_active);
+                    PopupWindow popupWindow = getPopUpView();
                 }
             });
         }
+
+        private PopupWindow getPopUpView() {
+            LayoutInflater inflater = (LayoutInflater)
+                    eventContext.getSystemService(LAYOUT_INFLATER_SERVICE);
+            View popupView = inflater.inflate(R.layout.popup_window, null);
+
+            int width = LinearLayout.LayoutParams.MATCH_PARENT;
+            int height = LinearLayout.LayoutParams.WRAP_CONTENT;
+            boolean focusable = true;
+            PopupWindow popupWindow = new PopupWindow(popupView, width, height, focusable);
+            popupWindow.setAnimationStyle(R.style.popup_window_animation);
+            popupWindow.showAtLocation(itemView, Gravity.BOTTOM, 0, 0);
+            return popupWindow;
+        }
     }
+
+
 }
